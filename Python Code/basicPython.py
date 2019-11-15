8d52338f936fcfc2c8a44772c3aeb9d882fe4d40
ValentinCiceu <officialval111@gmail.com>
2019-11-15 19:33:55 +0000

    python 1

---
+++ Python Code/basicPython.py
@@ -0,0 +1,15 @@
+# The following code is of week one of python
+print("Hello my name is George!")
+
+print("This is my dog Max!")
+print('o----')
+print(' ||||')
+
+# some maths!
+print(50 - 10)
+
+# let's make a variable for year I was born in by inputting!
+# make sure to click on the black screen to type an press enter!
+year = input("What year are you born in? ")
+# Now lets see how old I am!
+print(2019 - year)
